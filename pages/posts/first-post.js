07ea8a1e3b66e303db4f49c582736d56f1cf2721
chika-1127 <rejuvenator@hotmail.co.jp>
2021-06-02 22:12:25 +0900

    画像追加＆Imageコンポーネント使用

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import Image from "next/image";
 
 export default function FirstPost() {
   return (
@@ -9,6 +10,7 @@ export default function FirstPost() {
           <a>Back to home</a>
         </Link>
       </h2>
+      <Image src="/images/profile.png" height={455} width={690} alt="Pikachu" />
     </>
   );
 }
