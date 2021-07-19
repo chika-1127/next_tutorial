08e541714a8c4fcf2615a19bae055e36f71957e3
chika-1127 <rejuvenator@hotmail.co.jp>
2021-07-20 00:17:05 +0900

    投稿ページにtitleを追加、日付のフォーマット

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,5 +1,20 @@
 import Layout from "../../components/layout";
 import { getAllPostIds, getPostData } from "../../lib/posts";
+// Add this import
+import Head from "next/head";
+
+export default function Post({ postData }) {
+  return (
+    <Layout>
+      {/* Add this <Head> tag */}
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+
+      {/* Keep the existing code here */}
+    </Layout>
+  );
+}
 
 export async function getStaticProps({ params }) {
   const postData = await getPostData(params.id);
@@ -16,18 +31,4 @@ export async function getStaticPaths() {
     paths,
     fallback: false,
   };
-}
-
-export default function Post({ postData }) {
-  return (
-    <Layout>
-      {postData.title}
-      <br />
-      {postData.id}
-      <br />
-      {postData.date}
-      <br />
-      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
-    </Layout>
-  );
 }
