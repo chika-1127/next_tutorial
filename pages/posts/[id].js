7ff15978ad14c12eab0af06293a1dc70e3a42840
chika-1127 <rejuvenator@hotmail.co.jp>
2021-07-20 00:21:47 +0900

    投稿ページを磨き上げる

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,17 +1,22 @@
 import Layout from "../../components/layout";
 import { getAllPostIds, getPostData } from "../../lib/posts";
-// Add this import
 import Head from "next/head";
+import Date from "../../components/date";
+import utilStyles from "../../styles/utils.module.css";
 
 export default function Post({ postData }) {
   return (
     <Layout>
-      {/* Add this <Head> tag */}
       <Head>
         <title>{postData.title}</title>
       </Head>
-
-      {/* Keep the existing code here */}
+      <article>
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
     </Layout>
   );
 }
